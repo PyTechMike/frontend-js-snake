d1f37345efc1de853a16268991e59b28bfe65658
PyTechMike <technomike04@gmail.com>
2018-02-11 17:27:23 +0200

    continue testing with module snake

--- src/classes/snake.js
+++ src/classes/snake.js
@@ -9,9 +9,10 @@ class Snake {
 	}
 	move(steps) {
 		steps = steps || 1;
-		this.parts.forEach(function (i) {
+		this.parts.forEach(function (name, i) {
 			if (i === 0) {
-				this.parts[i].move(steps);
+				this.head.direction = this.direction;
+				this.head.move(steps);
 			} else {
 				this.parts[i].direction = this.parts[i - 1].direction;
 				this.parts[i].move(steps);
@@ -21,8 +22,9 @@ class Snake {
 		});
 	}
 	eat() {
-		this.parts.push(SnakePart);
-		this.parts[parts.length - 1].direction = this.parts[parts.length - 2].direction;
+		var newPart = new SnakePart({ direction: this.parts[parts.length - 2].direction });
+		this.parts.push(newPart);
+		//this.parts[parts.length - 1].direction = this.parts[parts.length - 2].direction;
 
 	}
 }
