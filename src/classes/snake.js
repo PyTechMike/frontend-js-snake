3c277c094b713278871697cd6cf4b8f38c208006
PyTechMike <technomike04@gmail.com>
2018-02-11 18:23:23 +0200

    And more updates

--- src/classes/snake.js
+++ src/classes/snake.js
@@ -9,22 +9,31 @@ class Snake {
 	}
 	move(steps) {
 		steps = steps || 1;
-		this.parts.forEach(function (name, i) {
+		this.parts.forEach(function (part, i) {
 			if (i === 0) {
 				this.head.direction = this.direction;
 				this.head.move(steps);
 			} else {
-				this.parts[i].direction = this.parts[i - 1].direction;
-				this.parts[i].move(steps);
+				part.direction = this.parts[i - 1].direction;
+				part.move(steps);
 
 			}
 
 		});
 	}
 	eat() {
-		var newPart = new SnakePart({ direction: this.parts[parts.length - 2].direction });
-		this.parts.push(newPart);
-		//this.parts[parts.length - 1].direction = this.parts[parts.length - 2].direction;
+		if (this.parts.length === 0) {
+			var needToAdd = this.parts.length;
+			while (needToAdd !== 0) {
+				var lastPart = this.parts[parts.length - 1];
+				var newPart = new SnakePart({ direction: lastPart.direction });
+				this.parts.push(newPart);
+			}
+		} else if (this.parts.length > 0) {
+			var lastPart = this.parts[parts.length - 1];
+			var newPart = new SnakePart({ direction: lastPart.direction });
+			this.parts.push(newPart);
+		}
 
 	}
 }
